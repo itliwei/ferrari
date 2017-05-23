75f87cc65863b2df524cf1066b3b1c2a643e94e8
王婷婷 <wangtt86@ziroom.com>
2017-05-23 19:25:07 +0800

    项目初框架

---
+++ src/main/java/com/ziroom/ferrari/producter/controller/ProducerController.java
@@ -0,0 +1,60 @@
+package com.ziroom.ferrari.producter.controller;
+
+import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
+import com.alibaba.rocketmq.client.producer.SendResult;
+import com.alibaba.rocketmq.common.message.Message;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author wangtt
+ * @date 2017/5/23 19:13
+ * @desc
+ * @since 1.0
+ */
+@RestController
+public class ProducerController {
+
+    @RequestMapping(value = "/producer", method = RequestMethod.GET)
+    public void produer(){
+        DefaultMQProducer producer = new DefaultMQProducer("test_group_name");
+        try{
+            /**
+             * 设置nameserver
+             * 默认使用http://jmenv.tbsite.net:8080/rocketmq/nsaddr 自动寻址,只要对应的机器配置hosts即可,开发环境的hosts配置如下
+             * 10.16.37.108 jmenv.tbsite.net
+             * 也可以通过jvm启动参数配置域名
+             * -Drocketmq.namesrv.domain=mynameserver.domain
+             * 若都没有配置请使用如下方式设置
+             */
+            producer.setNamesrvAddr("10.16.37.109:9876");
+//        producer.setVipChannelEnabled(false);
+            producer.start();
+
+            for (int i = 0; i < 1000; i++) {
+                try {
+                    Message msg = new Message("TopicTest",// topic
+                            "TagA",// tag
+                            ("Hello RocketMQ " + i).getBytes()// body
+                    );
+                    SendResult sendResult = producer.send(msg);
+                    System.out.println(sendResult);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                    Thread.sleep(1000);
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            producer.shutdown();
+        }
+    }
+
+
+
+
+
+}
---
+++ src/main/java/controller/consumer.java
@@ -0,0 +1,51 @@
+package controller;
+
+import com.alibaba.rocketmq.client.consumer.DefaultMQPushConsumer;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.common.consumer.ConsumeFromWhere;
+import com.alibaba.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+/**
+ * @Author wangtt
+ * @date 2017/5/23 14:16
+ * @desc
+ * @since 1.0
+ */
+public class consumer {
+    public static void main(String[] args) throws InterruptedException, MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("consumer");
+        /**
+         * 设置nameserver
+         * 默认使用http://jmenv.tbsite.net:8080/rocketmq/nsaddr 自动寻址,只要对应的机器配置hosts即可,开发环境的hosts配置如下
+         * 10.16.37.108 jmenv.tbsite.net
+         * 也可以通过jvm启动参数配置域名
+         * -Drocketmq.namesrv.domain=mynameserver.domain
+         * 若都没有配置请使用如下方式设置
+         */
+        consumer.setNamesrvAddr("10.16.37.109:9876");
+
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+
+        //设置topic及过滤tag，多个tag使用‘||’分隔
+        consumer.subscribe("TopicTest", "*");
+
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                                                            ConsumeConcurrentlyContext context) {
+                System.out.println(Thread.currentThread().getName() + " Receive New Messages: " + msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        consumer.start();
+
+        System.out.println("Consumer Started.");
+    }
+}
