63acb1c5c4183439983fec6958b7a1041c6b45bc
dongh38 <Dh13623652058>
2017-05-26 17:45:37 +0800

    sharding config ,waiting which field to splitting.

---
+++ ferrari-core/src/main/java/com/ziroom/ferrari/algorithm/MessageKeyTableAlgorithm.java
@@ -0,0 +1,9 @@
+package com.ziroom.ferrari.algorithm;
+
+/**
+ * @author dongh38@ziroom.com
+ * @version 1.0.0
+ * declare how to split tables.
+ */
+public class MessageKeyTableAlgorithm {
+}
---
+++ ferrari-core/src/main/java/com/ziroom/ferrari/config/ShardingConfiguration.java
@@ -0,0 +1,15 @@
+package com.ziroom.ferrari.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author dongh38@ziroom.com
+ * @version 1.0.0
+ * sharding configuration
+ */
+@Configuration
+public class ShardingConfiguration {
+
+
+
+}
