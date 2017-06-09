99ff2988c3c7ceaeb38e65e3efea61838e262226
liwei <liw411@ziroom.com>
2017-06-09 12:39:34 +0800

    修改

--- src/main/java/com/ziroom/ferrari/task/MessageWorkerQueue.java
+++ src/main/java/com/ziroom/ferrari/task/MessageWorkerQueue.java
@@ -4,6 +4,7 @@ import com.ziroom.ferrari.exception.DataChangeMessageSendException;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashSet;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
@@ -12,7 +13,7 @@ import java.util.concurrent.TimeUnit;
  * Created by liwei on 2017/6/7 0007.
  */
 @Slf4j
-public class MessageWorkerQueue extends PriorityBlockingQueue<DataChangeMessageWorker> {
+public class MessageWorkerQueue extends PriorityBlockingQueue<Runnable> {
     private static final int DEFAULT_INITIAL_CAPACITY = 20;
 
     private transient HashSet<String> keySet = new HashSet<>();
@@ -26,20 +27,22 @@ public class MessageWorkerQueue extends PriorityBlockingQueue<DataChangeMessageW
     }
     /**
      * 放置元素
-     * @param worker DataChangeMessageWorker
+     * @param runnable DataChangeMessageWorker
      * @throws DataChangeMessageSendException
      */
-    public void put(DataChangeMessageWorker worker) {
+    public void put(Runnable runnable) {
+        DataChangeMessageWorker worker = (DataChangeMessageWorker) runnable;
         if(this.keySet.add(worker.getDataChangeMessageEntity().getMsgId())) {
             super.put(worker);
         }
     }
     /**
      * 放置元素
-     * @param worker DataChangeMessageWorker
+     * @param runnable Runnable
      * @throws DataChangeMessageSendException
      */
-    public boolean offer(DataChangeMessageWorker worker) {
+    public boolean offer(Runnable runnable) {
+        DataChangeMessageWorker worker = (DataChangeMessageWorker) runnable;
         log.info("MessageWorkerQueue offer :"+worker.getDataChangeMessageEntity());
         if (this.size()>DEFAULT_INITIAL_CAPACITY){
             throw new DataChangeMessageSendException("队列任务数超出最大数");
@@ -56,7 +59,7 @@ public class MessageWorkerQueue extends PriorityBlockingQueue<DataChangeMessageW
      * @throws InterruptedException
      */
     public DataChangeMessageWorker take() throws InterruptedException {
-        DataChangeMessageWorker dataChangeMessageQueueTask = super.take();
+        DataChangeMessageWorker dataChangeMessageQueueTask = (DataChangeMessageWorker)super.take();
         if(dataChangeMessageQueueTask != null) {
             this.keySet.remove(dataChangeMessageQueueTask.getDataChangeMessageEntity().getMsgId());
         }
@@ -65,7 +68,7 @@ public class MessageWorkerQueue extends PriorityBlockingQueue<DataChangeMessageW
     }
 
     public DataChangeMessageWorker poll(long timeout, TimeUnit unit) throws InterruptedException {
-        DataChangeMessageWorker dataChangeMessageQueueTask = super.poll(timeout, unit);
+        DataChangeMessageWorker dataChangeMessageQueueTask = (DataChangeMessageWorker)super.poll(timeout, unit);
         if(dataChangeMessageQueueTask != null) {
             this.keySet.remove(dataChangeMessageQueueTask.getDataChangeMessageEntity().getMsgId());
         }
