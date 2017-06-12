e6a37f2aa0a14d4b6ea57470b40573e342e76b52
zhout38 <zhout38@ziroom.com>
2017-06-12 15:00:17 +0800

    注册中心初始化

---
+++ src/main/java/com/ziroom/ferrari/config/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package com.ziroom.ferrari.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Application Config
+ * Created by cheshun on 2016/4/12.
+ */
+@Configuration
+@ComponentScan(basePackages = "com.ziroom.ferrari")
+public class ApplicationConfig {
+}
